97772b12d9435c33e9ba54956382917ff4b5947c
Marcos Tognetti Baradelli <marcostognetti123@gmail.comm>
2023-01-09 15:07:03 -0300

    first commit

---
+++ src/modules/cars/repositories/PostgresCategoriesRepositories.ts
@@ -0,0 +1,20 @@
+import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  IcreateCategoryDTO,
+} from "./ICategoriesRepository";
+
+class PostgresCategoriesRepository implements ICategoriesRepository {
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+  list(): Category[] {
+    return null;
+  }
+  create({ description, name }: IcreateCategoryDTO) {
+    console.log(name, description);
+  }
+}
+
+export { PostgresCategoriesRepository };
